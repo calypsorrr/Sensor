44905bf673a0099c498f9318c0dec07a457bb4e5
calypsorrr <74073146+calypsorrr@users.noreply.github.com>
2021-03-21 12:04:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 import sensor
-import lorawan
-import wifi
 import time
-import wifiada
-from pysense import Pysense
 import machine
 
-if lorawan.connect():
-    while True:
-        sensor.readuart()
-        distance = sensor.readout()
-        if distance != False:
-            lorawan.send(distance)
-            machine.sleep(3000*10, True)
-            lorawan.connect()
-
-#wifi.wifi_connect()
-#while True:
-    #sensor.readuart()
-    #distance = sensor.readout()
-    #if distance != False:
-        #wifiada.sendDataWifi(distance)
+while True:
+    sensor.readuart()
+    distance = sensor.readout()
+    if distance != False:
+        print(distance)
