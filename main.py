a2983eb206eaff4b442b41fbf72aeecbc2a7896f
calypsorrr <74073146+calypsorrr@users.noreply.github.com>
2021-03-21 12:01:18 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-from machine import UART
+import sensor
+import lorawan
+import wifi
+import time
+import wifiada
+from pysense import Pysense
+import machine
 
-uart = UART(1)
-uart.init(9600, bits=8, parity=None, stop=1, timeout_chars=100, pins=('P3', 'P4'))
+if lorawan.connect():
+    while True:
+        sensor.readuart()
+        distance = sensor.readout()
+        if distance != False:
+            lorawan.send(distance)
+            machine.sleep(3000*10, True)
+            lorawan.connect()
 
-while True:
-    header_bytes = uart.read(1)
-    while(header_bytes != b'\xff'):
-        header_bytes=uart.read(1)
-    data_high = int(uart.read(1)[0])
-    data_low = int(uart.read(1)[0])
-    data_sum = int(uart.read(1)[0])
-    sum = data_high + data_low
-    if (sum == data_sum + 1):
-        distance = data_high*256 + data_low
-        distance_2 = data_high*256 + data_low
-        distance_3 = (distance_2)/10
-        DEG = "{:8.2f}".format(distance)
-        DEG_2 ="{:8.2f}".format(distance_3)
-        print("De afstand bedraagt" + DEG + " mm" + " | " + DEG_2 + " cm")
+#wifi.wifi_connect()
+#while True:
+    #sensor.readuart()
+    #distance = sensor.readout()
+    #if distance != False:
+        #wifiada.sendDataWifi(distance)
